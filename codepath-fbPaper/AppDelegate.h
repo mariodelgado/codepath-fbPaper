a8990d717abfefede9dd28bae0e543b345b67d10
Mario Delgado <mario.c.delgado@gmail.com>
2014-06-21 13:01:36 -0700

    Initial Commit

---
+++ codepath-fbPaper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  codepath-fbPaper
+//
+//  Created by Mario C. Delgado Jr. on 6/21/14.
+//  Copyright (c) 2014 Mario C. Delgado Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ codepath-fbPaperTests/codepath_fbPaperTests.m
@@ -0,0 +1,34 @@
+//
+//  codepath_fbPaperTests.m
+//  codepath-fbPaperTests
+//
+//  Created by Mario C. Delgado Jr. on 6/21/14.
+//  Copyright (c) 2014 Mario C. Delgado Jr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface codepath_fbPaperTests : XCTestCase
+
+@end
+
+@implementation codepath_fbPaperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
